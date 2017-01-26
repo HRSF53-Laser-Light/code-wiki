d20ab8e9cfd9a12397185c4f28dedef2422ada8f
Justin Mendelson <jmendelson22@gmail.com>
2017-01-26 12:42:49 -0800

    Webpack configuration

--- src/server/server.js
+++ src/server/server.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var db = require('../db/schema');
+var path = require('path');
 
 var app = express();
 
 // serve static client-facing files
-app.use(express.static(__dirname + '/src/public'));
+app.use(express.static(path.resolve(__dirname, '../public')));
 
 // spin up server
 app.listen('3000', function() {
