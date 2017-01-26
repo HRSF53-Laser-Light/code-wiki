d985bf1399a0ce7de4966fbd1d25a9bea0265625
Kay Christensen <kaychristensen@gmail.com>
2017-01-26 12:44:32 -0800

    Add more endpoints

--- src/server/routes.js
+++ src/server/routes.js
@@ -2,24 +2,29 @@ var router = require('express').Router();
 var controller = require('./controller');
 
 // Routes for signup
-router.get('/signup', controller.users.get); // serve signup page
-router.post('/signup', controller.users.post); // add a new user to the db
+// router.get('/signup', controller.signup.get); // serve signup page
+// router.post('/signup', controller.signup.post); // add a new user to the db
 
 // Routes for signin
-router.get('/signin', controller.users.get); // serve signin page
-router.post('/signin', controller.users.post);
+// router.get('/signin', controller.signin.get); // serve signin page
+// router.post('/signin', controller.signin.post);
 
 // Routes for homepage content
-router.get('/', controller.posts.get);
+router.get('/', function(req, res) {
+  res.send('this is the homepage');
+});
 
 // Routes for resource posts
-router.post('/post', controller.posts.post);
-// router.post('/delete', controller.delete.post); // delete a post from the db
+router.get('/posts', controller.posts.get);
+
+router.get('/delete', function(req, res) {
+  res.send('this deletes things');
+}); // delete a post from the db
 
 // Route for upvoting and downvoting
-router.post('/rank', controller.rank.post);
+// router.post('/rank', controller.rank.post);
 
-// Routes for tagging 
-router.post('/tag', controller.tag.post);
+// // Routes for tagging 
+// router.post('/tag', controller.tag.post);
 
 module.exports = router;
