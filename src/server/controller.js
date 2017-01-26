b6e9963b0d4cdbe185308dd3102d362d8e037a43
Kay Christensen <kaychristensen@gmail.com>
2017-01-26 01:41:59 -0800

    Create controller file

---
+++ src/server/controller.js
@@ -0,0 +1,40 @@
+var db = require('../db/schema');
+
+module.exports = {
+  users: {
+    get: function(req, res) {
+      // db.Users.findAll()
+      //   .then(function(users) {
+      //     res.json(users);
+      //   });
+    },
+    post: function(req, res) {
+      // db.Users.findOrCreate({where: {username: req.body.username}})
+      //   .spread(function(user, created) {
+      //     res.sendStatus(created ? 201 : 200);
+      //   });
+    }
+  },
+  posts: {
+    get: function(req, res) {
+
+    },
+    post: function(req, res) {
+      // db.Posts.findOrCreate({where: {username: req.body.username}})
+      //   .spread(function(user, created) {
+      //     db.Message.create({
+      //       problem_statement: req.body.problem_statement,
+      //       resource: req.body.resource,
+      //       vote_count: 0
+      //     }).then(function(post) {
+      //       res.sendStatus(201);
+      //     });
+      //   });
+    }
+  },
+  tags: {
+    post: function(req, res) {
+
+    }
+  }
+};
