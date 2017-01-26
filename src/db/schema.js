05179fb5012567c9863881574d110af34974f325
Justin Mendelson <jmendelson22@gmail.com>
2017-01-25 19:08:28 -0800

    db schema complete

--- src/db/schema.js
+++ src/db/schema.js
@@ -1,27 +1,32 @@
+var mysql = require('mysql');
 var Sequelize = require('sequelize');
 var sequelize = new Sequelize('wiki', 'root', '');
 
-var Users = sequelize.define('Users', {
+exports.Users = sequelize.define('Users', {
   username: Sequelize.STRING,
   password: Sequelize.STRING,
   salt: Sequelize.STRING
 });
 
-var Tags = sequelize.define('Tags', {
+exports.Tags = sequelize.define('Tags', {
   tag: Sequelize.STRING
 });
 
-
-var Posts = sequelize.define('Posts', {
+exports.Posts = sequelize.define('Posts', {
   problem_statement: Sequelize.STRING,
   resource: Sequelize.STRING,
   vote_count: Sequelize.INTEGER
 })
 
-var Category = sequelize.define('Category', {
+exports.Category = sequelize.define('Category', {
   name: Sequelize.STRING
 })
 
 Category.hasMany(Posts);
 Tags.belongsToMany(Posts, { through: 'Tags-Posts' });
 Posts.belongsToMany(Tags, { through: 'Tags-Posts' });
+
+Users.sync();
+Tags.sync();
+Posts.sync();
+Category.sync();
