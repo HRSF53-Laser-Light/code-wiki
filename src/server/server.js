999025ad37a72235ad4455423180d15664763d6e
Kay Christensen <kaychristensen@gmail.com>
2017-01-26 08:50:44 -0800

    Add routing

--- src/server/server.js
+++ src/server/server.js
@@ -3,12 +3,19 @@ var db = require('../db/schema');
 
 var app = express();
 
+// middleware
+var parser = require('body-parser');
+
+// // routing
+// var router = require('./routes');
+// app.use('', router);
+
 // serve static client-facing files
 app.use(express.static(__dirname + '/src/public'));
 
 // spin up server
 app.listen('3000', function() {
-  console.log('Listening on port 3000');
+  console.log('Listening like a boss on port 3000');
 });
 
 // export app
