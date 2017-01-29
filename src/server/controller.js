2bbaabb70ae257da2637943c90c02e0dbe4e186d
Kay Christensen <kaychristensen@gmail.com>
2017-01-29 15:33:15 -0800

    Push latest controller v

--- src/server/controller.js
+++ src/server/controller.js
@@ -1,64 +1,87 @@
 var db = require('../db/schema');
 
 module.exports = {
-  users: {
-    get: function(req, res) {
-      // db.Users.findAll()
-      //   .then(function(users) {
-      //     res.json(users);
-      //   });
-      res.send('welcome to controller users.get');
-    },
+  signup: {
     post: function(req, res) {
-      // db.Users.findOrCreate({where: {username: req.body.username}})
-      //   .spread(function(user, created) {
-      //     res.sendStatus(created ? 201 : 200);
-      //   });
-      res.send('welcome to controller users.post');
+      // hash password supplied by user
+      // post username and hashed password to database
+      // set up session
+
+      // res.send('controller signup.post');
+    }
+  },
+  signin: {
+    post: function(req, res) {
+      // get username and password from db
+        // if it's not there
+          // redirect to signup pag
+        // if it's there
+          // bcrypt compare username and password from signin and from db
+          // if they match
+            // set up a session
+          // if they don't match
+            // send back to signin page and encourage to sign in again
+
+      // res.send('controller signin.post');
+    }
+  },
+  signout: {
+    post: function(req, res) {
+      // reset session
+
+      // res.send('controller signout.post');
     }
   },
   posts: {
     get: function(req, res) {
-      res.send('welcome to controller posts.get');
-    },
-    post: function(req, res) {
-      // db.Posts.findOrCreate({where: {username: req.body.username}})
-      //   .spread(function(user, created) {
-      //     db.Message.create({
-      //       problem_statement: req.body.problem_statement,
-      //       resource: req.body.resource,
-      //       vote_count: 0
-      //     }).then(function(post) {
-      //       res.sendStatus(201);
-      //     });
-      //   });
-      res.send('welcome to controller posts.post');
+      // retrieve all posts in posts table
+      // filter to latest 10 results
+
+      // res.send('controller posts.get');
     }
   },
   tags: {
+    get: function(req, res) {
+      // retrieve all tags from tags table
+
+      // res.send('controller tags.post');
+    }
+  },
+  categories: {
+    get: function(req, res) {
+      // retrieve all categories from categories table
+
+      // res.send('controller categories.get');
+    }
+  },
+  submit: {
     post: function(req, res) {
-      res.send('welcome to controller tags.post');
+      // add a newly created post to the posts table
+        // link, comment, tags, category
+        // initialize votes to 0
+
+      // res.send('controller submit.post');
     }
   },
   delete: {
     post: function(req, res) {
-      res.send('welcome to controller delete.post');
+      // remove post from posts table
+
+      // res.send('controller delete.post');
     }
   },
-  signup: {
-    get: function(req, res) {
-      res.send('wecome to controller signup.get');
-    }, 
+  upvote: {
     post: function(req, res) {
-      res.send('wecome to controller signup.post');
+      // increment 'votes' on post in posts table
+
+      // res.send('controller upvote.post');
     }
   },
-  signout: {
-    get: function(req, res) {
-      res.send('wecome to controller signout.get');
-    }, 
+  downvote: {
     post: function(req, res) {
-      res.send('wecome to controller signout.post');
+      // decrement 'votes' on post in posts table
+
+      // res.send('controller downvote.post');
     }
   }
 };
