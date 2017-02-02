987150c650118a14762e60208383720dde8ec647
Brenner Spear <blspear@gmail.com>
2017-02-02 15:06:53 -0800

    update guest view & try to remove bundle.js

--- src/app/components/guestView/guestView.jsx
+++ src/app/components/guestView/guestView.jsx
@@ -14,52 +14,43 @@ export default class GuestView extends React.Component {
     });
   }
 
-  newUserView() {
+  newUserForm() {
     return (
-      <div className="container">
-        <div className="row">
-          <div className="col-sm-6 col-sm-offset-3">
-            <form className="sign-in">
-              <div className="form-group">
-                <input id="username" type="text" className="form-control" placeholder="username" />
-                <input id="password" type="text" className="form-control" placeholder="password" />
-                <input id="password2" type="text" className="form-control" placeholder="re-enter password" />
-              </div>
-              <button type="submit" className="btn btn-default">Sign Up</button>
-              <a href="#" onClick={()=>this.toggleView()}>Sign In</a>
-            </form>
-          </div>
+      <form className="sign-in">
+        <div className="form-group">
+          <input id="username" type="text" className="form-control" placeholder="username" />
+          <input id="password" type="text" className="form-control" placeholder="password" />
+          <input id="password2" type="text" className="form-control" placeholder="re-enter password" />
         </div>
-      </div>
+        <button type="submit" className="btn btn-default">Sign Up</button>
+        <a href="#" onClick={()=>this.toggleView()}>Sign In</a>
+      </form>
     );
   }
 
-  existingUserView() {
+  existingUserForm() {
     return (
-      <div className="container">
-        <div className="row">
-          <div className="col-sm-6 col-sm-offset-3">
-            <form className="sign-in">
-              <div className="form-group">
-                <input id="username" type="text" className="form-control" placeholder="username" />
-                <input id="password" type="text" className="form-control" placeholder="password" />
-              </div>
-              <button type="submit" className="btn btn-default">Sign In</button>
-              <a href="#" onClick={()=>this.toggleView()}>Sign Up</a>
-            </form>
-          </div>
+      <form className="sign-in">
+        <div className="form-group">
+          <input id="username" type="text" className="form-control" placeholder="username" />
+          <input id="password" type="text" className="form-control" placeholder="password" />
         </div>
-      </div>
+        <button type="submit" className="btn btn-default">Sign In</button>
+        <a href="#" onClick={()=>this.toggleView()}>Sign Up</a>
+      </form>
     );
   }
 
   render() {
-    if(this.state.newUser) {
-      return this.newUserView();
-    }
-    else {
-      return this.existingUserView();
-    }
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-6 col-sm-offset-3">
+            {(this.state.newUser) ? this.newUserForm() : this.existingUserForm()}
+          </div>
+        </div>
+      </div>
+    );
   }
 
 }
