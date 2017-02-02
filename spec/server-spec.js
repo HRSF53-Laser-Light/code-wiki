ff4138964bc5e02b3fad8877ecbab6592bd2d4fd
Kay Christensen <kaychristensen@gmail.com>
2017-02-02 14:38:17 -0800

    Sign in tests

--- spec/server-spec.js
+++ spec/server-spec.js
@@ -1,5 +1,6 @@
 var expect = require('chai').expect;
 var request = require('request');
+var bcrypt = require('bcrypt');
 
 var db = require('../src/db/schema');
 var controller = require('../src/server/controller');
@@ -9,177 +10,251 @@ var app = require('../src/server/server');
 
 var beforeEach = function() {};
 
+describe('', function() {
 
-describe('Sign up', function() {
   var server;
 
   before(function() {
-    server = app.listen(3000, function() {
-      console.log('Test is listening on 3000');
+    server = app.listen(4568, function() {
+      console.log('Test server is listening on 4568');
     });
+
+    db.User.destroy({
+        where: { username: 'sterlingarcher' }
+      });
   });
 
   after(function() {
     server.close();
   });
 
-  describe('Adds username to the database on sign up', function() {
+  beforeEach(function() {
+    // log out currently signed in user, if any
+    request('http://127.0.0.1:4568/api/signout', function(err, res, body) {});
 
-  });
-
-  describe('Adds hashed password to database on sign up', function() {
-
-  });
-
-  describe('Indicates when username is already taken', function() {
-
-  });
-
-  describe('Redirects to sign in page when user enters account info in database', function() {
-
-  });
-});
-
-describe('Sign in', function() {
-  var server;
-
-  before(function() {
-    server = app.listen(3000, function() {
-      console.log('Test is listening on 3000');
+    // remove Sterling Archer from db
+    db.User.destroy({
+      where: { username: 'sterlingarcher' }
+    }).then(function(results) {
+      console.log('results', results);
     });
   });
 
-  after(function() {
-    server.close();
+
+  describe('Sign up:', function() {
+
+    it('Adds username to the database on sign up', function(done) {
+      var options = {
+        'method': 'POST',
+        'followAllRedirects': true,
+        'uri': 'http://127.0.0.1:4568/api/signup',
+        'json': {
+          'username': 'sterlingarcher',
+          'password': 'phrasing'
+        }
+      };
+
+      request(options, function(error, res, body) {
+        // find db record for sterlingarcher
+        db.User.findAll({
+          where: { username: 'sterlingarcher' }
+        }).then(function(results) {
+          var foundUsername = results[0].dataValues.username;
+          // verify that db username is username entered through sign up page
+          expect(foundUsername).to.equal('sterlingarcher');
+          done();
+        });
+      });
+    });
+
+    it('Adds hashed password to database on sign up', function(done) {
+      // find db record for sterlingarcher
+      db.User.findAll({
+        where: { username: 'sterlingarcher' }
+      }).then(function(results) {
+        var foundPassword = results[0].dataValues.password;
+        // verify that stored pw is not plaintext
+        expect(foundPassword).to.not.equal('phrasing');
+
+        // verifies that plaintext password matches stored hash
+        bcrypt.compare('phrasing', foundPassword, function(err, comparison) {
+          if (err) {
+            console.log('Error in comparison', err);
+          } else {
+            expect(comparison).to.equal(true);
+            done();
+          }
+        });
+      });
+    });
+
+    // it('Indicates when username is already taken', function(done) {
+
+    // });
+
+    // it('Tells user to use sign in page when user enters account info in database', function(done) {
+
+    // });
   });
 
-  describe('Prompts to correct password if stored username is entered but password does not match', function() {
+  describe('Sign in:', function() {
+    db.User.create({
+      username: 'sterlingarcher',
+      password: 'phrasing'
+    });
 
-  });
+    it('Prompts user to correct password if stored username is entered but password does not match', function(done) {
+      var options = {
+        'method': 'POST',
+        'uri': 'http://127.0.0.1:4568/api/signin',
+        'json': {
+          'username': 'sterlingarcher',
+          'password': 'lana'
+        }
+      };
 
-  describe('Redirects to home page if correct username and password info entered', function() {
+      request(options, function(err, res, body) {
+        expect(res.statusCode).to.equal(404);
+        done();
+      });
+    });
 
-  });
-});
+    it('Redirects to home page if correct username and password info entered', function(done) {
+      var options = {
+        'method': 'POST',
+        'uri': 'http://127.0.0.1:4568/api/signin',
+        'json': {
+          'username': 'sterlingarcher',
+          'password': 'phrasing'
+        }
+      }
 
-describe('Sign out', function() {
-  var server;
-
-  before(function() {
-    server = app.listen(3000, function() {
-      console.log('Test is listening on 3000');
+      request(options, function(err, res, body) {
+        expect(res.statusCode).to.equal(200);
+        done();
+      });
     });
   });
 
-  after(function() {
-    server.close();
-  });
+  describe('Sign out:', function() {
+    var server;
 
-  describe('Redirects to sign out page', function() {
+    before(function() {
+      server = app.listen(4568, function() {
+        console.log('Test is listening on 4568');
+      });
+    });
 
-  });
-});
+    after(function() {
+      server.close();
+    });
 
-describe('Posting', function() {
-  var server;
+    it('Redirects to sign out page', function(done) {
 
-  before(function() {
-    server = app.listen(3000, function() {
-      console.log('Test is listening on 3000');
     });
   });
 
-  after(function() {
-    server.close();
-  });
+  describe('Posting:', function() {
+    var server;
 
-  describe('Retrieves latest 10 posts from database for frontend to render', function() {
+    before(function() {
+      server = app.listen(4568, function() {
+        console.log('Test is listening on 4568');
+      });
+    });
 
-  });
+    after(function() {
+      server.close();
+    });
 
-  describe('Adds brand new post to database', function() {
+    it('Retrieves latest 10 posts from database for frontend to render', function(done) {
 
-  });
+    });
 
-  describe('Does not add identical posts to database', function() {
+    it('Adds brand new post to database', function(done) {
 
-  });
+    });
 
-  describe('Deletes post from database when user removes post', function() {
+    it('Does not add identical posts to database', function(done) {
 
-  });
-});
+    });
 
-describe('Tagging', function() {
-  var server;
+    it('Deletes post from database when user removes post', function(done) {
 
-  before(function() {
-    server = app.listen(3000, function() {
-      console.log('Test is listening on 3000');
     });
   });
 
-  after(function() {
-    server.close();
-  });
+  describe('Tagging:', function() {
+    var server;
 
-  describe('Retrieves all created tags for frontend render', function() {
+    before(function() {
+      server = app.listen(4568, function() {
+        console.log('Test is listening on 4568');
+      });
+    });
 
-  });
+    after(function() {
+      server.close();
+    });
 
-  describe('Adds correct tags to post', function() {
+    it('Retrieves all created tags for frontend render', function(done) {
 
-  });
+    });
 
-  describe('Deletes tag from post in database when user removes tag', function() {
+    it('Adds correct tags to post', function(done) {
 
-  });
-});
+    });
 
-describe('Categories', function() {
-  var server;
+    it('Deletes tag from post in database when user removes tag', function(done) {
 
-  before(function() {
-    server = app.listen(3000, function() {
-      console.log('Test is listening on 3000');
     });
   });
 
-  after(function() {
-    server.close();
-  });
+  describe('Categories:', function() {
+    var server;
 
-  describe('Correctly adds category to post', function() {
+    before(function() {
+      server = app.listen(4568, function() {
+        console.log('Test is listening on 4568');
+      });
+    });
 
-  });
+    after(function() {
+      server.close();
+    });
 
-  describe('Can add brand new category', function() {
+    it('Correctly adds category to post', function(done) {
 
-  });
-});
+    });
 
-describe('Ranking', function() {
-  var server;
+    it('Can add brand new category', function(done) {
 
-  before(function() {
-    server = app.listen(3000, function() {
-      console.log('Test is listening on 3000');
     });
   });
 
-  after(function() {
-    server.close();
+  describe('Ranking:', function() {
+    var server;
+
+    before(function() {
+      server = app.listen(4568, function() {
+        console.log('Test is listening on 4568');
+      });
+    });
+
+    after(function() {
+      server.close();
+    });
+
+    it('Adds one to vote count when user upvotes post', function(done) {
+
+    });
+
+    it('Subtracts one from vote count when user downvotes post', function(done) {
+
+    });
+
+    it('Decrements vote count even when vote is zero or negative', function(done) {
+
+    });
   });
-
-  describe('Adds one to vote count when user upvotes post', function() {
-
-  });
-
-  describe('Subtracts one from vote count when user downvotes post', function() {
-
-  });
-
-  describe('Decrements vote count even when vote is zero or negative', function() {
-
-  });
-});
+})
