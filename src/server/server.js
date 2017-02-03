117f698945a70a61331b1d65568de48ef92d6841
Kay Christensen <kaychristensen@gmail.com>
2017-02-03 11:46:15 -0800

    Use express-sessions

--- src/server/server.js
+++ src/server/server.js
@@ -1,19 +1,31 @@
 var express = require('express');
-var db = require('../db/schema');
+var parser = require('body-parser');
+var session = require('express-session');
 var path = require('path');
+
+var db = require('../db/schema');
+var router = require('./routes');
 
 var app = express();
 
-// middleware - parsing
-var parser = require('body-parser');
+// middleware - parse JSON
 app.use(parser.json());
 
+// middleware - parse forms
+app.use(parser.urlencoded({ extended: true }));
+
 // routing
-var router = require('./routes');
 app.use('', router);
 
 // serve static client-facing files
 app.use(express.static(path.resolve(__dirname, '../public')));
+
+// set up authentication session
+app.use(session({
+  secret: 'saucefi3ld2o8',
+  resave: false,
+  saveUnitialized: true
+}))
 
 // spin up server
 app.listen('3000', function() {
