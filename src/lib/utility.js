40ef98efb49e06b1744804ce8c939f560801b447
Kay Christensen <kaychristensen@gmail.com>
2017-02-03 14:27:43 -0800

    Comments in utility functions

--- src/lib/utility.js
+++ src/lib/utility.js
@@ -1,10 +1,13 @@
 var isLoggedIn = function(req) {
+  console.log('req session', req.session);
+  console.log('req session user', req.session.user);
   return req.session ? !!req.session.user : false;
 };
 
 module.exports.checkUser = function(req, res, next){
   if (!isLoggedIn(req)) {
-    // User must be redirected to signin page
+    console.log('not logged in');
+    // User must be redirected to Signin page
     res.sendStatus(401);
   } else {
     next();
@@ -13,8 +16,9 @@ module.exports.checkUser = function(req, res, next){
 
 module.exports.createSession = function(req, res, newUser) {
   return req.session.regenerate(function() {
-      req.session.user = newUser;
-      // Send 201: Created status and new user's username back to client
-      res.send(201, { username: newUser.dataValues.username });
-    });
+    req.session.user = newUser;
+    console.log('req.session is', req.session)
+    console.log('req.session.user', req.session.user);
+    res.status(201).send({ username: newUser.dataValues.username });
+  });
 };
