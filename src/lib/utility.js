1b050bfded40d228b2230930b1b3bd9d63713ebc
Kay Christensen <kaychristensen@gmail.com>
2017-02-04 16:53:07 -0800

    Finalize util

--- src/lib/utility.js
+++ src/lib/utility.js
@@ -1,13 +1,9 @@
 var isLoggedIn = function(req) {
-  console.log('isLoggedIn req', req);
-  // console.log('isLoggedIn req', req);
   return req.session ? !!req.session.user : false;
 };
 
 module.exports.checkUser = function(req, res, next){
-  // console.log('checkUser req is', req);
   if (!isLoggedIn(req)) {
-    console.log('not logged in');
     // User must be redirected to Signin page
     res.sendStatus(401);
   } else {
@@ -17,9 +13,7 @@ module.exports.checkUser = function(req, res, next){
 
 module.exports.createSession = function(req, res, newUser) {
   return req.session.regenerate(function() {
-    req.session.user = newUser;
-    console.log('req.session is', req.session)
-    console.log('req.session.user', req.session.user);
+    req.session.user = newUser.dataValues;
     res.status(201).send({
       username: newUser.dataValues.username,
       userId: newUser.dataValues.id });
