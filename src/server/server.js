b32c7cd3ce7d857dd3d118e2aeddebe813312da3
Kay Christensen <kaychristensen@gmail.com>
2017-02-04 16:27:17 -0800

    Ugh

--- src/server/server.js
+++ src/server/server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var parser = require('body-parser');
+var mysql = require('mysql');
 var session = require('express-session');
+var MySQLStore = require('express-mysql-session')(session);
 var path = require('path');
 
 var db = require('../db/schema');
@@ -14,11 +16,24 @@ app.use(parser.json());
 // middleware - parse forms
 app.use(parser.urlencoded({ extended: true }));
 
+var options = {
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: '',
+    database: 'wiki'
+};
+
+var connection = mysql.createConnection(options); // or mysql.createPool(options); 
+var sessionStore = new MySQLStore({}/* session store options */, connection);
+
 // set up authentication session
 app.use(session({
+  key: 'app.sess',
   secret: 'saucecat',
   resave: false,
-  saveUnitialized: true,
+  saveUninitialized: true,
+  store: sessionStore,
   cookie: {
     secure: true, 
     maxAge: 3600000 
